02d8ff947130fc08e52f387c2b1c7b628d3c8398
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-04-10 23:45:23 +0200

    Create StockMarket Api Interface

---
+++ app/src/main/java/com/stock/market/data/remote/StockApi.kt
@@ -0,0 +1,18 @@
+package com.stock.market.data.remote
+
+import okhttp3.ResponseBody
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface StockApi {
+
+    @GET("query?function=LISTING_STATUS")
+    suspend fun getListings(
+        @Query("apikey") apiKey: String
+    ): ResponseBody
+
+    companion object {
+        const val BASE_URL = "https://www.alphavantage.co"
+        const val API_KEY = "Q7RM399EA59DQ6KE"
+    }
+}
