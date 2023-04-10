c230f16176a15755189e7f35e1f23fc512d91e10
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-04-10 23:44:54 +0200

    Create CompanyMapper.kt

---
+++ app/src/main/java/com/stock/market/data/mappers/CompanyMapper.kt
@@ -0,0 +1,20 @@
+package com.stock.market.data.mappers
+
+import com.stock.market.data.local.CompanyListingEntity
+import com.stock.market.domain.model.CompanyListing
+
+fun CompanyListingEntity.toCompanyListing(): CompanyListing {
+    return CompanyListing(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
+
+fun CompanyListing.toCompanyListingEntity(): CompanyListingEntity {
+    return CompanyListingEntity(
+        name = name,
+        symbol = symbol,
+        exchange = exchange
+    )
+}
