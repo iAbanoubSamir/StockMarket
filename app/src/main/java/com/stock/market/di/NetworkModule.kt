964d2457a903bd995b7fffd9f184934b47615486
Abanoub Samir <abanoub.sameer.22@gmail.com>
2023-06-21 00:37:14 +0300

    Create NetworkModule.kt

---
+++ app/src/main/java/com/stock/market/di/NetworkModule.kt
@@ -0,0 +1,26 @@
+package com.stock.market.di
+
+import com.stock.market.data.remote.StockApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideStockApi(): StockApi {
+        return Retrofit.Builder()
+            .baseUrl(StockApi.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create()
+    }
+}
